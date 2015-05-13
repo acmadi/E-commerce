c294e6331488b00ac74400d6cfc179d1ed214c79
rafal <r.jaworski@tourmedica.pl>
2015-05-13 18:35:08 +0200

    DoctrineMigrationsBundle generated

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new ProductBundle\ProductBundle(),
             new CategoryBundle\CategoryBundle(),
             new UserBundle\UserBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
